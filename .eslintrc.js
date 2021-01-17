5a7f1d04eaae32fa084f5432adbe713c1e3c3f52
Tairon Coelho <tairon.neitzel@g.globo>
2021-01-17 20:51:09 -0300

    Add prettier eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier', 'jest', 'cypress'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+};
